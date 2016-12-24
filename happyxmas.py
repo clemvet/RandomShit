6a8a71bd9f8c4f6e0599ebf27d411488dca7f217
clemvet <cr7.vet@gmail.com>
2016-12-24 17:57:47 +0100

    Update happyxmas.py

--- happyxmas.py
+++ happyxmas.py
@@ -1,4 +1,4 @@
-import time
+from time import sleep
 from webbrowser import open
 
 print "Have a christmas."
@@ -11,7 +11,7 @@ _|  _|\___|_|   _|   \__, |  \____|_| |_|_|   _|____/\__|_|  _|  _|\__,_|____/
 """
 
 print art
-time.sleep(4)
+sleep(4)
 
 for fucks in range(42):
     open("https://www.youtube.com/watch?v=6rqBCHVbH2E")
