2d40410aed740d9088daf9f0fa308cf796d87de8
clemvet <cr7.vet@gmail.com>
2016-12-24 17:59:25 +0100

    Add files via upload

---
+++ happyxmas.py
@@ -0,0 +1,18 @@
+from time import sleep
+from webbrowser import open
+
+print "Have a christmas."
+
+art = """\
+ \  |                         ___| |         _)      |
+ |\/ |  _ \  __|  __| |   |   |     __ \   __| |  __| __| __ `__ \   _` |  __|
+ |   |  __/ |    |    |   |   |     | | | |    |\__ \ |   |   |   | (   |\__
+_|  _|\___|_|   _|   \__, |  \____|_| |_|_|   _|____/\__|_|  _|  _|\__,_|____/
+"""
+
+print art
+sleep(4)
+
+for fucks in range(42):
+    open("https://www.youtube.com/watch?v=6rqBCHVbH2E")
+    sleep(10)
